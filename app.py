e3e438a0a7e9c711e766c8504a9b180f69f0fbb1
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-05-08 10:15:30 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world
     return """Hello, World! from github  {
   "db_user": "vamsi",
-  "db_pass": "securepassword-rollback",
+  "db_pass": "securepassword-rollback-2",
   "api_key": "your-api-key-value"
 } """
 
